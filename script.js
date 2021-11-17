e8c3d21ce5f06501cd6d0e3647208627af9b9f94
Chikage Molander <chikage.t.molander@gmail.com>
2021-11-17 21:25:04 +0100

    I cannot get the other js-file!!!!

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+// get students.js <---------doesn't work 😩 How do I get the other js-file? 
+const getFriends = (callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', () => { 
+     if(request.readyState === 4 && request.status === 200){
+       callback();
+     } else if(request.readyState === 4){
+       callback();
+     }
+  });
+  request.open("GET", "students.js");
+  request.send();
+};
 
+
+// shuffle students
+const shuffleArray = (array) => {
+  for (let i = array.length -1; 1 >0; i--) {
+    const j = Math.floor(Math.rancom() * (1 + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+shuffleArray(students);
 
