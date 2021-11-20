9f01f515af00e001d62fedb1dc4bffaab31c97ae
Chikage Molander <chikage.t.molander@gmail.com>
2021-11-20 11:45:18 +0100

    A chaotic Saturday morning

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const scoreEl = document.querySelector('#score');
 const imagEl = document.querySelector('#foto_container');
 const answerButtonsEl = document.querySelector('.answers');
 
@@ -16,15 +17,33 @@ shuffleArray(students);
 // output for test
 //console.log(students[1].name); // it's works!👍
 
+
+// insert and count points
+const uppdateScore = (correct) => {
+  scoreEl.innerHTML = `You are correct for <strong>${correct}</strong> of 10 questions!`;
+};
+
+
+
+// start new Game
+const startNewGame = () => {
+  //
+  correctAnswer = 
+
+  // reset score
+  correct = 0;
+}
+uppdateScore();
+
 // insert foto in HTML           <-------------------------
 imagEl.innerHTML = `<img class="col-12" src=${students[0].image}>`; //show up a random picture of array[0] 
-let collectAnswer = students[0].name
-// output for test if image has collect students name
+let correctAnswer = students[0].name
+// output for test if image has correct students name
 //console.log(students[0].name); // it's works!👍
 
 
 // insert answer button (atudents name) in HTML
-//shuffule a collect name and 3 more random names.
+//shuffule a correct name and 3 more random names.
 let answers = [];
 for(i = 0; i < 4; i++ ){
   answers.push(students[i].name);  
@@ -46,19 +65,19 @@ answerButtonsEl.addEventListener('click', e=> {
   //output for controll 'click'
   //console.log(`clicked ${e.target.tagName}`,e.target);
   
-  //if answer is collect
-  if(e.target.textContent === students[0].name){
-    console.log("Collect! 🥳"); 
+  //if answer is correct
+  if(e.target.textContent === correctAnswer){
+    console.log("Correct! 🥳");
 
   } else {
     console.log(" 😩 ");
   }
 
 });
+// use 'filter' and 'map' to check correct/ wrong answer.
 
-// use 'filter' and 'map' to check collect/ wrong answer.
 
-// insert points
-document.querySelector('#point').innerHTML = `<p id="score" class="text-center">YOU HAVE COLLECT <strong>X</strong> OF 10!</p>`;
+
+
 
 // insert cheat-button
